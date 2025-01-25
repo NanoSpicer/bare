51ffdf59c6e23bbea5a7196446315b18903b63f8
NanoSpicer <miquelrc95@outlook.es>
2025-01-25 20:50:11 +0100

    feat: Adds an environment variable to locate the SQLite Db file

--- src/configuration.ts
+++ src/configuration.ts
@@ -5,6 +5,10 @@ export const configuration = {
   AUTH_USERNAME: process.env.AUTH_USERNAME,
   AUTH_PASSWORD: process.env.AUTH_PASSWORD,
   APP_ROOT: process.env.APP_ROOT,
+  /**
+   * The path to the database file
+   */
+  DB_PATH: process.env.DB_PATH ?? 'sqlite.db',
 } as const 
 
 
--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
 import * as schema from './database/schema'
+import { configuration } from '@configuration';
 
-const sqlite = new Database('sqlite.db');
+const sqlite = new Database(configuration.DB_PATH)
 export const db = drizzle({ client: sqlite, schema });
